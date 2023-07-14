563a0fb97a2d8219d87e6a2cd7907f12d074eef5
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-07-14 11:33:56 +0700

    update hashSet implementation

--- dsa/leet-code/hash-table/hash-set/hashSet.ts
+++ dsa/leet-code/hash-table/hash-set/hashSet.ts
@@ -2,74 +2,57 @@
 class MyHashSet {
   //     collision resolution algoritm?
   //     implement hash fn?
-      private _buckets: number[]
-      private _tableSize: number
-  
-      constructor() {
-          this._buckets = new Array()
-          // Choose 5 as table size b/c 5 is a prime number
-          this._tableSize = 1013
-      }
-  
-      add(key: number): void {
-          let index = this.resolveCollision(key)
-          this._buckets[index] = key
-      }
-  
-      remove(key: number): void {
-          let index = this.resolveCollision(key)
-          this._buckets.splice(index, index + 1)
-      }
-  
-      contains(key: number): boolean {
-          let index = this.hash(key)
-          return this._buckets[index] ? true : false
-      }
-  
-      private resolveCollision(key: number, tableSize = this._tableSize): number {
-          // Calculate the hash value of the key.
-          const hashValue = this.hash(key)
-  
-          // Initialize the index to the hash value.
-          let index = hashValue;
-  
-          // if position of the key value is already occupied
-          if (this._buckets[index]) {
-              // duplicated key values --> do nothing
-              if (this._buckets[index] === key) {
-                  return index
-              }
-              index = hashValue + (tableSize * hashValue)
-              return index
-          }
-  
-          // The bucket is empty, so we can store the key here.
-          return index;
-      }
-  
-      private hash(value: number, tableSize = this._tableSize) {
-          /*
-              implement the hash function with modulo method
-          */
-          let hashValue: number = value % tableSize
-          
-          return hashValue
-      }
+  private _buckets: number[][]
+  private _tableSize: number
+
+  constructor() {
+    this._buckets = new Array()
+    // tableSize = 1013 --> arbitrary choice
+    // (plus it's a prime number)
+    this._tableSize = 1013
+    for (let i = 0; i < this._tableSize; i++) {
+      this._buckets.push([])
+    }
   }
-  
-  /**
-   * Your MyHashSet object will be instantiated and called as such:
-   * var obj = new MyHashSet()
-   * obj.add(key)
-   * obj.remove(key)
-   * var param_3 = obj.contains(key)
-   */
-  
-   var obj = new MyHashSet()
-  //  obj.add(6)
-  //  obj.remove(2)
-  //  console.log('obj', obj)
-  // var param_3 = obj.contains(1)
-  //  console.log('param_3', param_3)
-  
-  
+
+  add(key: number): void {
+    const index = this.hash(key)
+    if (this.contains(key)) {
+      this._buckets[index].push(key)
+    }
+  }
+
+  remove(key: number): void {
+    const index = this.hash(key)
+    const bucket = this._buckets[index]
+    const keyIndex = bucket.indexOf(key)
+    if (keyIndex > -1) {
+      bucket.splice(keyIndex, 1)
+    }
+  }
+
+  contains(key: number): boolean {
+    const index = this.hash(key)
+    const bucket = this._buckets[index]
+    return bucket.includes(key) ? true : false
+  }
+
+  private hash(value: number) {
+    return value % this._tableSize
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
+
+var obj = new MyHashSet()
+//  obj.add(6)
+//  obj.remove(2)
+//  console.log('obj', obj)
+// var param_3 = obj.contains(1)
+//  console.log('param_3', param_3)
